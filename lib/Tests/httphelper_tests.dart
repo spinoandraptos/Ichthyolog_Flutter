bc49df322741d61c2464566bd09640b8f5cc6617
spinoandraptos <spinoandraptos@gmail.com>
2023-06-21 14:52:22 +0800

    Added tests folder

---
+++ lib/Tests/httphelper_tests.dart
@@ -0,0 +1,22 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+import 'package:test/test.dart';
+import '../Helpers/http.dart';
+
+void main() {
+  final httpHelpers = HttpHelpers();
+
+  test('Test user should successfully sign up', () {
+    int testerNumber = Random().nextInt(20);
+    Future signupFuture = httpHelpers.signupRequest(
+        'AutomatedTester$testerNumber',
+        'AutomatedTester$testerNumber!!!',
+        'automatedTester$testerNumber@gmail.com');
+    signupFuture.then(
+      (response) {
+        expect(response, 'Signup Successful');
+      },
+    );
+    expect(signupFuture, completes);
+  });
+}
